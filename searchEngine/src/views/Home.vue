a9791ce4da1be208459807df9efc3377e8f843ac
jeyapriya <jeyapriya.mouttou@gmail.com>
2025-02-06 13:37:06 +0100

    feat  input  search by title of thr books

--- searchEngine/src/views/Home.vue
+++ searchEngine/src/views/Home.vue
@@ -4,30 +4,36 @@
       <Icon icon="material-symbols-light:book-5" class="text-6xl text-primary-500" />
       <p class="text-6xl font-k2d">Bookseek</p>
     </div>
-    <Search />
+
+    <!-- Search Component (Dropdown with Titles) -->
+    <Input v-model:search="searchQuery" @updateResults="updateBooks" />
   </main>
-<div class="container mx-auto p-4">
-  <div class="grid grid-cols-3 gap-6">
-    <div class="" v-for="book in booksStore.books" :key="book.id">
-      <BookCard :book=book  />
+
+  <!-- Display Matching Book Cards -->
+  <div class="container mx-auto p-4">
+    <div v-if="filteredBooks.length" class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <BookCard v-for="book in filteredBooks" :key="book.id" :book="book" />
     </div>
   </div>
-</div>
-
 </template>
 
 <script setup lang="ts">
-import Search from '@/components/search.vue';
+import Input from '../components/input.vue';
 import { Icon } from "@iconify/vue";
 import { ref, onMounted } from 'vue';
 import { useBooksStore } from '@/stores/books';
 import BookCard from '@/components/bookCard.vue';
 
 const booksStore = useBooksStore();
-
-const input = ref('')
+const searchQuery = ref("");
+const filteredBooks = ref([]); // Store list of matching books
 
 onMounted(() => {
   booksStore.fetchBooks();
 });
+
+// Update displayed books when the search results change
+const updateBooks = (books: any[]) => {
+  filteredBooks.value = books;
+};
 </script>
